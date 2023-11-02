0f7e33f35003233da7323a8ed073e8448cce8bc5
Yusuf Renkoglu <yusuf@renkoglu.com>
2023-11-02 19:00:57 +0200

    1. gun 1. ders

---
+++ 01-Variables.py
@@ -0,0 +1,9 @@
+isim = "Yusuf"
+yas = 39
+floatDegisken = 3.9
+booleanDegisken = True
+
+
+isim = 39
+
+print(type(isim))
---
+++ 02-Stringler.py
@@ -0,0 +1,22 @@
+metin = "Merhaba dunya!"
+
+yeniDegisken = metin.split() # Liste verir => ["Merhaba", "dunya!"]
+
+print(metin.upper()) # Buyuk harfe cevirir
+print(metin.replace("M", "Y")) # M harfini Y harfine cevirdik
+
+print(yeniDegisken)
+print(type(yeniDegisken)) # type fonksiyonu ile degiskenin tipini ogreniriz.
+
+print(metin[1]) # indexi 1 olan degeri verir
+
+# metin[baslangic:bitis:kacarKacar]
+print(metin[0:10:2]) # Mraad
+print(metin[3:]) #haba dunya!
+print(metin[:4]) # Merh
+print(metin[::2])
+
+#sonEleman = len(metin)-1
+print(metin[-2])
+
+print(len(metin)) # Degiskenin veya listenin uzunlugunu verir
